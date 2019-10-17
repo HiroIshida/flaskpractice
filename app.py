82f31ee43bfe305446b19f4e77748ea8788c9272
HiroIshida <spitfire.docomo@gmail.com>
2019-10-17 22:39:10 +0900

    show othero board

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
 from flask import *
 import json
+import numpy as np
 
 app = Flask(__name__)
 
-class Manager:
-    def __init__(self):
-        self.data_name = []
-        self.data_age = []
+class Othero:
+    def __init__(self, N_):
+        self.N = N_
+        self.board = [[None for i in range(N_)] for j in range(N_)]
 
-    def store(self, name, age):
-        self.data_name.append(name)
-        self.data_age.append(age)
-        self._dump()
+    def put(self, i, j, stone):
+        self.board[i, j] = stone
 
-    def _dump(self):
-        d = {"name": self.data_name, "age": self.data_age}
-        f = open("tmp.json", 'w')
-        json.dump(d, f, indent=4)
-        f.close()
+    def encord_html(self):
+        str_html = "<body>"
+        str_html += "<table class=\"table\">"
+        for i in range(self.N):
+            str_html += "<tr>"
+            for j in range(self.N):
+                if self.board[i][j] is not None:
+                    str_html += ("<td>" + self.board[i][j] + "</td>")
+                else:
+                    str_html += ("<td>" + "</td>")
+            str_html += "</tr>"
+        str_html += "</table>"
+        str_html += "</body>"
+        return str_html
 
-m = Manager()
+global str_header 
+f = open('index.html', 'r')
+str_header = f.read()
+f.close()
+
+global ot
+ot = Othero(10)
 
 @app.route("/", methods = ["GET", "POST"])
 def main():
-    message = "enter your name here:"
-    return render_template('index.html', message = message)
+    global ot
+    global str_header
+    str_body = ot.encord_html()
+    print str_body
+    str_html = "<html>" + str_header + str_body + "</html>"
+    return str_html
 
-@app.route("/fuck", methods = ["GET", "POST"])
-def fuck():
-    name = request.form["name"]
-    age = request.form["age"]
-    global m
-    m.store(name, age)
-    message = "name: " + name + " age: " + age + "\n"
-    message += "enter your name here:"
-    if request.method == "POST":
-        return render_template('index.html', message = message)
 
 if __name__ == "__main__":  
     app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
